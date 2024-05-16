e38a14ea27de5eb3b3e34355ae5c138094664fab
prakashrubal <prakashrubal@gmail.com>
2024-05-16 23:36:41 +0530

    added 4 ec2 instance in main.tf (configuration file) to test terraform state lock as code which I pulled in my ec2 had only 1 instance details in configuration; ran terraform apply -auto-approve from my ec2 when my instance were being created from local code and got error acquiring state lock in ec2 instance connect

--- day5_statefile_remote_config_and_statelock/main.tf
+++ day5_statefile_remote_config_and_statelock/main.tf
@@ -8,3 +8,49 @@ resource "aws_instance" "dev" {
     Name = "custom-ec2"
   }
 }
+
+# created 4 copies of above instance to test terraform state lock
+
+resource "aws_instance" "dev1" {
+  ami           = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name      = "custom-key"
+  subnet_id     = "subnet-068f499711005b222"
+  associate_public_ip_address = true
+  tags = {
+    Name = "test1-ec2"
+  }
+}
+
+resource "aws_instance" "dev2" {
+  ami           = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name      = "custom-key"
+  subnet_id     = "subnet-068f499711005b222"
+  associate_public_ip_address = true
+  tags = {
+    Name = "test2-ec2"
+  }
+}
+
+resource "aws_instance" "dev3" {
+  ami           = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name      = "custom-key"
+  subnet_id     = "subnet-068f499711005b222"
+  associate_public_ip_address = true
+  tags = {
+    Name = "test3-ec2"
+  }
+}
+
+resource "aws_instance" "dev4" {
+  ami           = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name      = "custom-key"
+  subnet_id     = "subnet-068f499711005b222"
+  associate_public_ip_address = true
+  tags = {
+    Name = "test4-ec2"
+  }
+}
