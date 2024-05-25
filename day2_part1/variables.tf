5c6905767f09b41ca158298e60f05b8e1f73c84b
Rubal Prakash <54391108+prakashrubal@users.noreply.github.com>
2024-05-25 23:27:38 +0530

    Update variables.tf for aws account 2 - since started jenkins pipeline using t2.medium instance

--- day2_part1/variables.tf
+++ day2_part1/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
     description = "we give the value of ami"
     type = string
-    default = "ami-013e83f579886baeb"
+    default = "ami-0cc9838aa7ab1dce7"
   
 }
 
@@ -12,16 +12,27 @@ variable "instance_type" {
   
 }
 
+#For AWS account 1
+#variable "key_name" {
+#    description = "we give the value of key"
+#    type = string
+#    default = "custom-key"
+#  
+#}
+
+#For AWS account 2
 variable "key_name" {
     description = "we give the value of key"
     type = string
-    default = "custom-key"
+    default = "key-profile2"
   
 }
 
+#For AWS account 1
 variable "subnet_id" {
     description = "we give the value of subnet id here"
     type = string
     default = "subnet-068f499711005b222"
-  
 }
+
+#For AWS account 2 nothing is required for subnet it will take default
