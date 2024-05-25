9dfbf72bb56913abf487e7d740aee6bf3302c878
Rubal Prakash <54391108+prakashrubal@users.noreply.github.com>
2024-05-25 23:29:58 +0530

    Update main.tf for jenkins pipeline testing

--- day2_part1/main.tf
+++ day2_part1/main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "dev" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    subnet_id = var.subnet_id
+# For aws account 1 subnet is needed
+#    subnet_id = var.subnet_id
+# For aws account 2 subnet is not needed, hence commented
     tags = {
       Name = "custom-ec2-terra"
     }
